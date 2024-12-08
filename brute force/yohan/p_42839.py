b3d64c9c6481f519b1463f6be106b2760559dba6
ramyo564 <yohan032yohan@gmail.com>
2024-12-09 00:26:28 +0900

    완전탐색 - 프로그래머스

---
+++ brute force/yohan/p_42839.py
@@ -0,0 +1,11 @@
+# https://school.programmers.co.kr/learn/courses/30/lessons/42839
+
+numbers = "17"
+# 3
+numbers = "011"
+# 2
+
+
+def solution(numbers):
+    answer = 0
+    return answer
---
+++ brute force/yohan/p_84512.py
@@ -0,0 +1,38 @@
+# https://school.programmers.co.kr/learn/courses/30/lessons/84512
+
+word = "AAAAE"
+
+def solution(word):
+    AEIOU = "AEIOU"
+    cnt = 0
+    for a in AEIOU:
+        temp = a
+        cnt += 1
+        if temp == word:
+            return cnt
+
+        for e in AEIOU:
+            temp2 = temp + e
+            cnt += 1
+            if temp2 == word:
+                return cnt
+
+            for i in AEIOU:
+                temp3 = temp2 + i
+                cnt += 1
+                if temp3 == word:
+                    return cnt
+
+                for o in AEIOU:
+                    temp4 = temp3 + o
+                    cnt += 1
+                    if temp4 == word:
+                        return cnt
+
+                    for u in AEIOU:
+                        temp5 = temp4 + u
+                        cnt += 1
+                        if temp5 == word:
+                            return cnt
+    return cnt
+print(solution(word))
