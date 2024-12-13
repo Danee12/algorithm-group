5a57795ac41ae2201e688827d7712593ee49e8a5
ramyo564 <yohan032yohan@gmail.com>
2024-12-13 21:13:03 +0900

    DFS/BFS - p_43163 / p_43164

--- bfs_dfs/yohan/p_43163.py
+++ bfs_dfs/yohan/p_43163.py
@@ -1,13 +1,60 @@
 # https://school.programmers.co.kr/learn/courses/30/lessons/43163
 
-begin = "hit"
-target = "cog"
-words = ["hot", "dot", "dog", "lot", "log", "cog"]
+# begin = "hit"
+# target = "cog"
+# words = ["hot", "dot", "dog", "lot", "log", "cog"]
+#
+
+begin = "aab"
+target = "aba"
+words = ["abb", "aba"]
+# # 2
+
+# begin = "hit"
+# target = "hhh"
+# words = ["hhh", "hht"]
+# 2
+
+from collections import deque
 
 
 def solution(begin, target, words):
-    answer = 0
-    return answer
+    # words에 처음부터 단어가 없을 경우 0 리턴
+    if target not in words:
+        return 0
+
+    # 전체 노드 갯수
+    n = len(words)
+
+    # 방문 노드 기록
+    visited = [False] * n
+
+    queue = deque([[begin, 0]])
+
+    while queue:
+        temp, cnt = queue.popleft()
+        if temp == target:
+            return cnt
+
+        for i in range(n):
+            # 방문한 적이 없을 경우 실행
+            if not visited[i]:
+                difference = 0
+
+                # 현재의 단어가 바꿀 단어랑 몇 글자가 다른지 체크
+                for r in range(len(words[i])):
+                    if temp[r] != words[i][r]:
+                        difference += 1
+
+                # 1개만 다를 경우 바꿀 수 있으므로 1개만 다르면 바꿔줌
+                if difference == 1:
+                    next = words[i]
+
+                    # 바꾼 단어의 노드를 True 체크 -> 재방문 방지
+                    visited[i] = True
+
+                    # 방문횟수 카운팅 추가
+                    queue.append([next, cnt + 1])
 
 
 print(solution(begin, target, words))
--- bfs_dfs/yohan/p_43164.py
+++ bfs_dfs/yohan/p_43164.py
@@ -1,13 +1,43 @@
 # https://school.programmers.co.kr/learn/courses/30/lessons/43164
 
-tickets = [["ICN", "JFK"], ["HND", "IAD"], ["JFK", "HND"]]
-
-
+# tickets = [["ICN", "JFK"], ["HND", "IAD"], ["JFK", "HND"]]
 # ["ICN", "JFK", "HND", "IAD"]
+
+
+# tickets = [["ICN", "AAA"], ["ICN", "CCC"], ["CCC", "DDD"], ["AAA", "BBB"], ["AAA", "BBB"], ["DDD", "ICN"],
+#            ["BBB", "AAA"]]
+# # # 정답 ["ICN", "CCC", "DDD", "ICN", "AAA", "BBB", "AAA", "BBB"]
+
+tickets = [["ICN", "SFO"], ["ICN", "ATL"], ["SFO", "ATL"], ["ATL", "ICN"], ["ATL", "SFO"]]
+
+from collections import deque
+
 
 def solution(tickets):
     answer = []
-    return answer
+    q = deque()
+    q.append((["ICN"], []))
+
+    while q:
+        # 기록할 경로와 사용한 티켓 기록
+        path, used = q.popleft()
+
+        # 사용한 티켓의 수와 전체 티켓의 수가 같다면
+        # 모든 티켓을 사용했을 경우 answer에 넣어줌
+        if len(used) == len(tickets):
+            answer.append(path)
+
+        for idx, ticket in enumerate(tickets):
+            depart, arrived = ticket
+
+            # 출발점 유효성확인 및 티켓 사용 여부 확인
+            if depart == path[-1] and not idx in used:
+                q.append((path + [arrived], used + [idx]))
+
+    # 모든 경로를 순회한 조건의 경로들을 사전순으로 정렬
+    answer.sort()
+
+    return answer[0]
 
 
 print(solution(tickets))
