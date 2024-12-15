866714a11d5bf9210f4c668335e9eb28a50afb4e
ramyo564 <yohan032yohan@gmail.com>
2024-12-16 01:24:46 +0900

    Update p_42839.py

--- brute force/yohan/p_42839.py
+++ brute force/yohan/p_42839.py
@@ -2,10 +2,30 @@
 
 numbers = "17"
 # 3
-numbers = "011"
-# 2
+# numbers = "011"
+# # 2
 
+from itertools import permutations
+from math import sqrt
 
 def solution(numbers):
-    answer = 0
-    return answer
+    temp_set = set()
+    answer = set()
+    for i in range(1, len(numbers) + 1):
+        for temp in permutations(numbers, i):
+            temp_set.add(int("".join(temp)))
+
+    for i in temp_set:
+        if i > 1:  # 0과 1은 소수가 아님
+            is_prime = True
+            for j in range(2, int(sqrt(i)) + 1):
+                if i % j == 0:
+                    is_prime = False
+                    break
+            if is_prime:
+                answer.add(i)
+    return len(answer)
+
+
+
+print(solution(numbers))
